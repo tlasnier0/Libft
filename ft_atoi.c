05d2019c6d1e641f2a208513bce0c8f52fa0b56b
Tom LASNIER <tlasnier@2a01cb0013e04400e662530a25af10da.ipv6.abo.wanadoo.fr>
2026-09-19 19:00:46 +0200

    ft_atoi v1

---
+++ ft_atoi.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlasnier <tlasnier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/19 18:30:36 by tlasnier          #+#    #+#             */
+/*   Updated: 2026/09/19 18:55:44 by tlasnier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	result;
+	int	sign;
+
+	i = 0;
+	result = 0;
+	sign = 1;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - 48);
+		i++;
+	}
+	return (result * sign);
+}
+/* #include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+	(void) argc;
+	printf("   atoi: %d\nft_atoi: %d\n", atoi(argv[1]), ft_atoi(argv[1]));
+} */
