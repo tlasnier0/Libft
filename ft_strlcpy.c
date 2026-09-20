f4d0d8b17279b8da80deff5c26683ce8dcd4ab91
Tom LASNIER <to.lasnier@gmail.com>
2026-09-21 00:45:08 +0200

    add ft_strlcpy,c

---
+++ ft_strlcpy.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlasnier <tlasnier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/21 00:22:03 by tlasnier          #+#    #+#             */
+/*   Updated: 2026/09/21 00:44:13 by tlasnier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dest, const char *src, size_t length)
+{
+	int	i;
+
+	i = 0;
+	while (i < length - 1 && src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = 0;
+	return (ft_strlen(src));
+}
+
+/* #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+	char *toprint;
+	char *toprint2;
+
+	toprint = malloc(sizeof(char) * ft_atoi(argv[2]));
+	toprint2 = malloc(sizeof(char) * ft_atoi(argv[2]));
+	strlcpy(toprint, argv[1], ft_atoi(argv[3]));
+	ft_strlcpy(toprint2, argv[1], ft_atoi(argv[3]));
+	(void)argc;
+	printf("   strlcpy: %s\nft_strlcpy: %s\n", toprint, toprint2);
+} */
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tlasnier <tlasnier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/19 19:34:11 by tlasnier          #+#    #+#             */
-/*   Updated: 2026/09/20 23:28:16 by tlasnier         ###   ########.fr       */
+/*   Updated: 2026/09/21 00:44:37 by tlasnier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,6 @@ void	*ft_memset(void *ptr, int v, size_t length);
 void	ft_bzero(void *ptr, size_t length);
 void	*ft_memcpy(void *dest, const void *src, size_t length);
 void	*ft_memmove(void *dest, const void *src, size_t length);
+size_t	ft_strlcpy(char *dest, const char *src, size_t length);
 
 #endif
